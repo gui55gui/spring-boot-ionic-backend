c859486e4def90cec8000f0505057120db1b09ae
Guilherme Machado <guilherme.gmachado95@gmail.com>
2019-03-05 20:09:04 -0300

    Criação das entidades Estado e Cidade e seus repositórios.

--- src/main/java/com/guilherme/cursomc/CursomcApplication.java
+++ src/main/java/com/guilherme/cursomc/CursomcApplication.java
@@ -1,8 +1,12 @@
 package com.guilherme.cursomc;
 
 import com.guilherme.cursomc.domain.Categoria;
+import com.guilherme.cursomc.domain.Cidade;
+import com.guilherme.cursomc.domain.Estado;
 import com.guilherme.cursomc.domain.Produto;
 import com.guilherme.cursomc.repositories.CategoriaRepository;
+import com.guilherme.cursomc.repositories.CidadeRepository;
+import com.guilherme.cursomc.repositories.EstadoRepository;
 import com.guilherme.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -19,6 +23,10 @@ public class CursomcApplication implements CommandLineRunner {
     private CategoriaRepository categoriaRepository;
     @Autowired
     private ProdutoRepository produtoRepository;
+    @Autowired
+    private CidadeRepository cidadeRepository;
+    @Autowired
+    private EstadoRepository estadoRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
@@ -34,6 +42,18 @@ public class CursomcApplication implements CommandLineRunner {
         Produto p2 = new Produto(null, "Impressora", 800.00);
         Produto p3 = new Produto(null, "Mouse", 80.00);
 
+        Estado est1 = new Estado(null, "Santa Catarina");
+        Estado est2 = new Estado(null, "São Paulo");
+
+        Cidade cid1 = new Cidade(null, "Joinville", est1);
+        Cidade cid2 = new Cidade(null, "Itajai", est1);
+        Cidade cid3 = new Cidade(null, "Florianopólis", est1);
+        Cidade cid4 = new Cidade(null, "Guarulhos", est2);
+        Cidade cid5 = new Cidade(null, "Osasco", est2);
+
+        est1.getCidades().addAll(Arrays.asList(cid1, cid2, cid3));
+        est2.getCidades().addAll(Arrays.asList(cid4, cid5));
+
         cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
         cat2.getProdutos().addAll(Arrays.asList(p2));
 
@@ -43,6 +63,8 @@ public class CursomcApplication implements CommandLineRunner {
 
         categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
         produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+        estadoRepository.saveAll(Arrays.asList(est1, est2));
+        cidadeRepository.saveAll(Arrays.asList(cid1, cid2, cid3, cid4, cid5));
 
     }
 }
---
+++ src/main/java/com/guilherme/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.guilherme.cursomc.repositories;
+
+import com.guilherme.cursomc.domain.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+
+}
