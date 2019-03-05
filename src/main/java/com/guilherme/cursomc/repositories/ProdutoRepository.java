0ea22cd62bb226af4218100fb3cf830eae200b33
Guilherme Machado <guilherme.gmachado95@gmail.com>
2019-03-05 00:00:41 -0300

    Criação da entidade Produto. Criação do repository para Produto. Criação dos mapeamentos de Produto e relacionamento com Categoria. Criação das associações muitos pra muitos.

--- src/main/java/com/guilherme/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/guilherme/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,10 @@
 package com.guilherme.cursomc.repositories;
 
-import com.guilherme.cursomc.domain.Categoria;
+import com.guilherme.cursomc.domain.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
-
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
 }
