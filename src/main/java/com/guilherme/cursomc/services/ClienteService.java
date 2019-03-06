3f75d92194d493fe1a42b22c2b543084e52bf08f
Guilherme Machado <guilherme.gmachado95@gmail.com>
2019-03-05 22:47:16 -0300

    Criação do endpoint de clientes.

---
+++ src/main/java/com/guilherme/cursomc/services/ClienteService.java
@@ -0,0 +1,22 @@
+package com.guilherme.cursomc.services;
+
+import com.guilherme.cursomc.domain.Cliente;
+import com.guilherme.cursomc.repositories.ClienteRepository;
+import com.guilherme.cursomc.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repo;
+
+    public Cliente buscar(Integer id) {
+        Optional<Cliente> cliente = repo.findById(id);
+        return cliente.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+    }
+
+}
