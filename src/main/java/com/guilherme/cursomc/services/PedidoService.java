723e6052238195f90d2f498398ac69166f3ceffa
Guilherme Machado <guilherme.gmachado95@gmail.com>
2019-03-24 20:43:10 -0300

    Endpoint /pedidos/{id} disponibilizado.

---
+++ src/main/java/com/guilherme/cursomc/services/PedidoService.java
@@ -0,0 +1,22 @@
+package com.guilherme.cursomc.services;
+
+import com.guilherme.cursomc.domain.Pedido;
+import com.guilherme.cursomc.repositories.PedidoRepository;
+import com.guilherme.cursomc.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PedidoService {
+
+    @Autowired
+    private PedidoRepository repo;
+
+    public Pedido buscar(Integer id) {
+        Optional<Pedido> Pedido = repo.findById(id);
+        return Pedido.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+    }
+
+}
